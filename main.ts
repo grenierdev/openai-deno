3bc57deff217488550470dc95147e421d383334e
Michael Grenier <michael@grenier.dev>
2023-08-17 13:30:48 -0400

    chore: virtual receptionist

--- main.ts
+++ main.ts
@@ -1,111 +1,139 @@
 const openAIApiKey = Deno.env.get("OPENAI_API_KEY");
 
-async function* streamAsyncIterable(stream: ReadableStream<Uint8Array>) {
+const systemPrompt = `Tu incarnes Jocelyne, notre réceptionniste virtuelle. Tu dois rediriger les demandes aux bonnes personnes selon la discussion.
+
+Voici les services que nous offront :
+
+# Registraire
+Service : admission, inscription, reconnaissance des acquis
+Contact : Chantal notre experte au poste 8982
+
+# Services aux étudiants 
+Service : inscription, confort et plaisir d'étudier à l'université
+Contact : Pierre notre conseiller au poste 8743`;
+
+const textEncoder = new TextEncoder();
+
+const kv = await Deno.openKv();
+
+type Message = { role: string; content: string; };
+
+const server = Deno.serve(
+	{
+		port: 8080
+	},
+	async (req, info) => {
+		const sessionId = req.headers.get("X-DISCUSSION-ID") ?? crypto.randomUUID();
+		const history: Message[] = [];
+		for await (const message of await kv.list<Message>({ prefix: ["discussion", sessionId] })) {
+			history.push(message.value);
+		}
+		if (history.length == 0) {
+			const systemMessage = { role: "system", content: systemPrompt };
+			await kv.set(["discussion", sessionId, Date.now()], systemMessage);
+			history.push(systemMessage);
+		}
+		const message = { role: 'user', content: await req.text() };
+		await kv.set(["discussion", sessionId, Date.now()], message);
+		history.push(message);
+
+		console.log('History', history);
+
+
+		const response = await fetch("https://api.openai.com/v1/chat/completions", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": `Bearer ${openAIApiKey}`,
+			},
+			body: JSON.stringify({
+				model: "gpt-3.5-turbo",
+				temperature: 0,
+				stream: true,
+				functions: [
+					{
+						name: "forget_conversation",
+						description: "Oublier la conversation, effacer la conversation, efface mes données",
+						parameters: { type: "object", properties: { dummy: { type: "null" } } }
+					}
+				],
+				messages: [...history],
+			}),
+		});
+
+		const parser = parseOpenAIChatCompletionsStream(response.body!);
+		let assistantResponse = '';
+
+		return new Response(new ReadableStream({
+			async pull(controller) {
+				const chunk = await parser.next();
+				if (chunk.done) {
+					if (assistantResponse) {
+						await kv.set(["discussion", sessionId, Date.now()], { role: 'assistant', content: assistantResponse });
+					}
+					controller.enqueue(textEncoder.encode(`\n`));
+					controller.close();
+				} else {
+					if (chunk.value.type === "chunk") {
+						assistantResponse += chunk.value.content;
+						controller.enqueue(textEncoder.encode(chunk.value.content));
+					} else if (chunk.value.type === "function_call") {
+						if (chunk.value.name === "forget_conversation") {
+							controller.enqueue(textEncoder.encode("Tout est oublié."));
+							for await (const message of await kv.list({ prefix: ["discussion", sessionId] })) {
+								await kv.delete(message.key);
+							}
+							await kv.set(["discussion", sessionId, Date.now()], { role: "system", content: systemPrompt });
+						}
+					} else {
+						controller.enqueue(new Uint8Array())
+					}
+				}
+			}
+		}), { status: 200, headers: { "Content-Type": "text/plain; charset=utf-8" } });
+	}
+);
+
+await server.finished;
+
+type OpenAIChatCompletionChunk =
+	| { type: "chunk"; content: string }
+	| { type: "function_call"; name: string; arguments: unknown }
+
+async function* parseOpenAIChatCompletionsStream(stream: ReadableStream<Uint8Array>): AsyncGenerator<OpenAIChatCompletionChunk, void> {
+	const textDecoder = new TextDecoder();
 	const reader = stream.getReader();
 	try {
+		let rest = '';
 		while (true) {
 			const { done, value } = await reader.read();
 			if (done) return;
-			yield value;
+			const chunk = rest + textDecoder.decode(value);
+			let idx = 0;
+			for (const match of chunk.matchAll(/data: ([^\n]+)\n\n/g)) {
+				idx += match[0].length;
+				if (match[1] === "[DONE]") return;
+				try {
+					const json = JSON.parse(match[1]) as unknown;
+					if (json && typeof json === "object" && "object" in json && "choices" in json && Array.isArray(json.choices)) {
+						const choice = json.choices[0] as { index: number; finish_reason: null | string; delta: { content: string; role?: string; function_call?: { name: string; arguments: unknown } } };
+						if (choice.finish_reason) {
+							//yield { type: "stop" };
+						} else if (choice.delta.function_call) {
+							if (choice.delta.function_call.name) {
+								yield { type: "function_call", name: choice.delta.function_call.name, arguments: choice.delta.function_call.arguments };
+							}
+						} else {
+							yield { type: "chunk", content: choice.delta.content };
+						}
+					}
+				} catch (error) {
+					console.error(error);
+				}
+			}
+			rest = chunk.slice(idx);
 		}
 	} finally {
 		reader.releaseLock();
 	}
 }
-
-console.time("request time");
-const response = await fetch("https://api.openai.com/v1/chat/completions", {
-	method: "POST",
-	headers: {
-		"Content-Type": "application/json",
-		"Authorization": `Bearer ${openAIApiKey}`,
-	},
-	body: JSON.stringify({
-		model: "gpt-3.5-turbo",
-		temperature: 0.9,
-		stream: true,
-		messages: [
-			{ role: "system", content: `You are a helpful assistance.` },
-			{ role: "assistant", content: `How may I help you today?` },
-			{
-				role: "user",
-				content:
-					`Can you suggest me a title for an article about good fishing spot in Rouyn-Noranda? I only one the best suggestion you got.`,
-			},
-			{
-				role: "assistant",
-				content:
-					`Sure, here's a suggestion: "Reeling in the Best Catches: Exploring the Top Fishing Spots in Rouyn-Noranda".`,
-			},
-			{
-				role: "user",
-				content: `Write me an intro for that article, in french.`,
-			},
-		],
-	}),
-});
-console.timeEnd("request time");
-const textDecoder = new TextDecoder();
-const textEncoder = new TextEncoder();
-const dataLineRegEx = /data: (.+)\n/g;
-for await (const chunk of streamAsyncIterable(response.body!)) {
-	const message = textDecoder.decode(chunk);
-	try {
-		let match: RegExpExecArray | null = null;
-		while ((match = dataLineRegEx.exec(message))) {
-			const message = match[1];
-			if (message === "[DONE]") break;
-			const json = JSON.parse(message);
-			await Deno.stdout.write(textEncoder.encode(json?.choices?.[0]?.delta.content));
-		}
-	} catch (error) {
-		console.log();
-		console.error(error);
-		console.log(message);
-		break;
-	}
-}
-console.log();
-
-// console.time("chat");
-// const response = await fetch("https://api.openai.com/v1/chat/completions", {
-// 	method: "POST",
-// 	headers: {
-// 		"Content-Type": "application/json",
-// 		"Authorization": `Bearer ${openAIApiKey}`
-// 	},
-// 	body: JSON.stringify({
-// 		model: "gpt-3.5-turbo",
-// 		temperature: 0.9,
-// 		messages: [
-// 			{ role: "system", content: `You are a helpful assistance.` },
-// 			{ role: "assistant", content: `How may I help you today?` },
-// 			{ role: "user", content: `Can you suggest me a title for an article about good fishing spot in Rouyn-Noranda? I only one the best suggestion you got.` },
-// 			{ role: "assistant", content: `Sure, here's a suggestion: "Reeling in the Best Catches: Exploring the Top Fishing Spots in Rouyn-Noranda".` },
-// 			{ role: "user", content: `Write me an intro for that article, in french.` }
-// 		]
-// 	})
-// });
-// const json = await response.json();
-// console.timeEnd("chat");
-// console.log(json);
-// console.log(json?.choices?.[0]?.message.content);
-
-// import { ChatOpenAI } from "https://esm.sh/langchain@0.0.77/chat_models/openai.js";
-// import { AIChatMessage, HumanChatMessage, SystemChatMessage } from "https://esm.sh/langchain@0.0.77/schema.js";
-
-// const chat = new ChatOpenAI({
-// 	temperature: 0.9,
-// 	openAIApiKey,
-// });
-
-// console.time("chat");
-// const resposeA = await chat.call([
-// 	new SystemChatMessage(`You are a helpful assistance.`),
-// 	new AIChatMessage(`How may I help you today?`),
-// 	new HumanChatMessage(`Can you suggest me a title for an article about good fishing spot in Rouyn-Noranda? I only one the best suggestion you got.`),
-// 	new AIChatMessage(`Sure, here's a suggestion: "Reeling in the Best Catches: Exploring the Top Fishing Spots in Rouyn-Noranda".`),
-// 	new HumanChatMessage(`Write me an intro for that article, in french.`),
-// ]);
-// console.timeEnd("chat");
-// console.log(resposeA?.text);
